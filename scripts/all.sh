3f115914bcd5b27ac329c1e086c096dd4e399338
Kostas Apostolopoulos <kostas.apostolopoulos@vechain.org>
2023-10-27 18:10:53 +0100

    Update relative pathing

--- scripts/all.sh
+++ scripts/all.sh
@@ -2,8 +2,9 @@
 set -e
 
 scripts_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-"$scripts_path/clean.sh"
-"$scripts_path/build.sh"
-"$scripts_path/run.sh"
+cd $scripts_path
+./clean.sh
+./build.sh
+./run.sh
 sleep 1
-"$scripts_path/test.sh"
+./test.sh
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
 
+scripts_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $scripts_path
 docker build -t node-healthcheck:dev ..
