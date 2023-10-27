50759de5adc1100c3baf0e663483df08a34e71e2
Kostas Apostolopoulos <kostas.apostolopoulos@vechain.org>
2023-10-28 00:08:07 +0100

    Restore buildx multi-arch process

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,4 @@ set -e
 
 scripts_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $scripts_path
-docker build -t node-healthcheck:dev ..
+docker buildx build --platform linux/amd64,linux/arm64/v8 -t node-healthcheck:dev ..
